fec59b887c8bb18b66c438d7c3b3c817c3a85455
Tanaka Kent <tanaken0304.0123@gmail.com>
2021-01-16 17:41:09 +0900

    Finish to make dictionary

---
+++ prediction/make_dictionary.py
@@ -0,0 +1,27 @@
+import csv
+
+def main():
+    # tsv読み込み
+    with open('../output/df_output_analize.tsv')as f:
+        tsv_reader = csv.reader(f, delimiter='\t')
+        read_data = [row[2] for row in tsv_reader]
+
+    # 名詞と動詞のみの辞書作成
+    word2token = {}
+    token2word = {}
+    # トークン初期値
+    token = 1
+    for word in read_data:
+        if word in word2token:
+            continue
+        else:
+            word2token[word] = token
+            token2word[token] = word
+            token += 1
+
+    return word2token, token2word
+
+
+if __name__ == '__main__':
+    main()
+
