b80d9872f4cbb9d6995998aba97bdab07b7b41d0
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2021-01-16 23:39:23 +0900

    filled NA

--- fill_missing_value.R
+++ fill_missing_value.R
@@ -3,16 +3,18 @@
 library(tidyverse)
 library(data.table)
 
-rm(list = ls())
 
 # read data
-#df_sample = fread("./output/df_sample2.tsv", sep = "\t", encoding = "UTF-8", header = T)
-for(i in 1:nrow(df_sub) - 1) {
+df_sub = fread("./output/df_sub_filled.csv", sep = ",", encoding = "UTF-8", header = T)
+for(i in 1:(nrow(df_sub) - 1)) {
+  
   if(is.na(df_sub[i]$value_verb)){
+    # count
+    print(paste0("loop : ", i))
     df_sub[i]$value_verb = df_sub[i + 1]$value_verb
     df_sub[i]$tag_verb = df_sub[i + 1]$tag_verb
     df_sub[i]$order_verb = df_sub[i + 1]$order_verb
   }
 }
 
-write.table("./output/df_sub_filled.csv", row.names = F, col.names = T, sep = ",")
+write.table(df_sub,"./output/df_sub_filled.csv", row.names = F, col.names = T, sep = ",")
